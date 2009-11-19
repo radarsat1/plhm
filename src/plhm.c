35d9a0be89eb9213d9706cf41994f9c5036a9e94
Stephen Sinclair <sinclair@music.mcgill.ca>
2009-11-18 21:47:13 -0500

    Fix some unused variable warnings.

--- src/plhm.c
+++ src/plhm.c
@@ -288,18 +288,6 @@ int timeval_subtract (struct timeval *result,
                       const struct timeval *x,
                       const struct timeval *y)
 {
-    /* Perform the carry for the later subtraction by updating y. */
-    if (x->tv_usec < y->tv_usec) {
-        int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-//        y->tv_usec -= 1000000 * nsec;
-//        y->tv_sec += nsec;
-    }
-    if (x->tv_usec - y->tv_usec > 1000000) {
-        int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-//        y->tv_usec += 1000000 * nsec;
-//        y->tv_sec -= nsec;
-    }
-
     /* Compute the time remaining to wait.
        tv_usec is certainly positive. */
     result->tv_sec = x->tv_sec - y->tv_sec;
@@ -311,11 +299,7 @@ int timeval_subtract (struct timeval *result,
 
 int GetBinPno(polhemus_t *pol)
 {
-    const char *buf;
-    char hex[4000];
-    char tmp[10];
     int i = 0;
-    int br, count, start;
     struct timeval now, diff;
 
     static int c=0;
